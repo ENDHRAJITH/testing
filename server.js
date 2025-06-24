bcd9ac59b95d755645808cf18211eff3df53ecb3
Endhrajith T <endhrajitthiyagu@gmail.com>
2025-06-24 11:28:15 +0530

    Initialize the Basic Strucher  of the Folder

---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+
+import express from 'express';
+import mongoose from 'mongoose';
+ 
+import cors from 'cors';
+
+// Load env variables
+
+
+const app = express();
+const PORT =  5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+ 
+
+// Connect to MongoDB
+mongoose
+  .connect(`mongodb+srv://endhrajiththiyagu:end123@cluster0.3wu5ebk.mongodb.net/`)
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
